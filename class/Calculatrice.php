fcbdfc0dc357460478fec5e72fb728963da16d59
Zacharie-William Gagné <zwgagne@outlook.com>
2021-07-15 11:09:44 -0400

    added sqrt function

--- class/Calculatrice.php
+++ class/Calculatrice.php
@@ -1,38 +1,65 @@
 <?php
 
-class Calculatrice{
+class Calculatrice
+{
     public function calcul($entry = "2+3")
     {
-       return $this->calculate($entry);
+        return $this->calculate($entry);
     }
 
-    private function calculate($data){
+    private function calculate($data)
+    {
         $data_array = str_split($data);
         return $this->operation($data_array);
     }
-    public function operation($array){
+    public function operation($array)
+    {
         switch ($array[1]) {
             case "+":
                 return $this->addition($array[0], $array[2]);
                 break;
             case "-":
-                echo "i égal 1";
+                return $this->soustraction($array[0], $array[2]);
                 break;
             case "*":
-                echo "i égal 2";
+                return $this->multiplication($array[0], $array[2]);
                 break;
             case "/":
-                echo "i égal 2";
+                return $this->division($array[0], $array[2]);
                 break;
             case "%":
-                echo "i égal 2";
+                return $this->modulo($array[0], $array[2]);
+                break;
+            case "^":
+                return $this->racine($array[0]);
                 break;
             default:
                 break;
         }
     }
 
-    public function addition($left, $right){
-       return (int)$left + (int)$right;
+    public function addition($left, $right)
+    {
+        return (int)$left + (int)$right;
+    }
+    public function soustraction($left, $right)
+    {
+        return (int)$left - (int)$right;
+    }
+    public function multiplication($left, $right)
+    {
+        return (int)$left * (int)$right;
+    }
+    public function division($left, $right)
+    {
+        return (float)$left / (float)$right;
+    }
+    public function modulo($left, $right)
+    {
+        return (int)$left % (int)$right;
+    }
+    public function racine($left)
+    {
+        return sqrt((int)$left);
     }
 }
--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if(isset($_POST['count'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculatrice</title>
 </head>
 <body>
 <h1><?php echo $total; ?></h1>
